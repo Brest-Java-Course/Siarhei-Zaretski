d0ab136cd0bb262e0b99d50f371bea6299e1861a
sphincs <sphincs@tut.by>
2014-10-17 18:35:53 +0300

    2nd commit

---
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -0,0 +1,21 @@
+package com.epam.brest.courses.domain;
+
+public class User {
+
+	private Long userId;
+	
+	private String login;
+
+	private String userName;
+
+	public Long getUserId(){
+		return userId;
+	}
+
+	public void setUserId(Long userId){
+		this.userId = userId;
+	}
+
+	
+
+}
